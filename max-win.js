5d88ef9baeec23bd06f73eff4b3befbcd23578fc
Dennis Porter Jr <dennisporterjr@gmail.com>
2020-05-05 15:18:38 -0400

    messing with some more windows

--- max-win.js
+++ max-win.js
@@ -1,23 +1,29 @@
-var mxsw = function (nums, k) {
+var mxsw = function (nums, wdw) {
     var curr = 0, prev = 0, len = nums.length, i = 0, max = -Infinity;
-    for (; i < k; i++)
+    for (; i < wdw; i++)
         curr += nums[i];
-    for (; i < len; i++) {
-        prev = curr;
-        curr += nums[i] - nums[i - 3];
-        printmx(nums, k, i);
+    printmx(nums, wdw, 0, curr);
+    i = 1;
+    max = curr;
+    for (; i <= len - wdw; i++) {
+        var plus = nums[i + wdw - 1], minus = nums[i - 1];
+        curr += plus - minus;
+        max = Math.max(curr, max);
+        printmx(nums, wdw, i, curr + ",\tmax: " + max);
     }
-    return 'hey';
 };
-var printmx = function (arr, wdw, from) {
-    var out = "";
-    arr.forEach(function (elem, i) {
-        out += from >= i && i < (from + wdw) ? "." : "";
-        out += elem + "  ";
+var printmx = function (arr, wdw, from, sum) {
+    var out = from + ":\t";
+    arr.forEach(function (n, i) {
+        var begin = i == from;
+        var end = i == (from + wdw - 1);
+        out += begin ? "[" : " ";
+        out += n + (end ? "]" : " ") + "\t";
     });
+    out += "\t=> " + sum;
     console.log(out);
 };
-console.log(mxsw([4, 3, 7, -1, -3, 5, 3, 6], 3));
+console.log(mxsw([1, 3, 2, 2, 4, 3, 7, -1, -3, 5, 3, 6], 3));
 // [4  3  -1  -3] 5  3  6  7       4 (3)
 //  4 [3  -1  -3  5] 3  6  7       3 (-1)
 //  4  3 [-1  -3  5  3] 6  7       5 (5)
--- max-win.ts
+++ max-win.ts
@@ -1,32 +1,47 @@
-var mxsw = function(nums: number[], k: number) {
+var mxsw = function(nums: number[], wdw: number) {
     let curr = 0,
         prev = 0,
         len = nums.length,
         i = 0,
         max = -Infinity;
 
-    for(;i<k;i++) curr += nums[i];
+    for(;i<wdw;i++) curr += nums[i];
+    printmx(nums, wdw, 0, curr);
 
-    for(; i < len; i++) {
-        prev = curr;
-        curr += nums[i] - nums[i-k];
-        printmx(nums,k,i);
+    i = 1;
+    max = curr;
+
+    for(; i <= len-wdw; i++) {
+        let plus = nums[i+wdw-1],
+            minus = nums[i-1];
+        curr += plus - minus;
+        max = Math.max(curr, max);
+        printmx(nums, wdw, i, curr + ",\tmax: " + max);
     }
 
 };
 
-var printmx = function(arr: number[], wdw: number, from: number) {
-    let out = "";
+var printmx = function(
+        arr     : number[],
+        wdw     : number,
+        from    : number,
+        sum     : number|string,
+    ) {
+    let out = from + ":\t";
 
-    arr.forEach((elem, i) => {
-        out += from >= i && i < (from+wdw) ? "." : "";
-        out += elem + "  ";
+    arr.forEach((n, i) => {
+        let begin = i == from;
+        let end = i == (from+wdw-1);
+        out += begin ? "[" : " ";
+        out += n + (end ? "]" : " ") + "\t";
     });
 
+    out += "\t=> "  + sum;
     console.log(out);
+
 }
 
-console.log(mxsw([4,3,7,-1,-3,5,3,6], 3));
+console.log(mxsw([1, 3, 2, 2, 4,3,7,-1,-3,5,3,6], 3));
 
 
 // [4  3  -1  -3] 5  3  6  7       4 (3)
