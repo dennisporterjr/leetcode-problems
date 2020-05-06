c17e04326ccc124f312028c2fd77d2761ed89a7c
Dennis Porter Jr <dennisporterjr@gmail.com>
2020-05-06 10:56:48 -0400

    99th percentile

--- min-window-substring-optimized.js
+++ min-window-substring-optimized.js
@@ -1,155 +1,86 @@
-var jp = JSON.parse, js = JSON.stringify, l = console.log;
-var loop = 0;
 /**
  * @description https://leetcode.com/problems/minimum-window-substring/
+ * @repo https://github.com/dennisporterjr/practice-algorithms/blob/master/min-window-substring-optimized.js
  * @param {string} s
  * @param {string} t
  * @return {string}
  */
-var minWindow = function(s, t, log) {
-    loop = 0;
+var minWindow = function(s, t) {
 
+    // check if we need to do any work at all...
     if(s.length === 0 || t.length === 0) return '';
 
-    var table = Array(128).fill(0),
-        fslen,
-        tlen = t.length,
-        ans = '',
-        fs = [],
-        right = 0,
-        left = 0,
-        count = 0;
+    // define some vars
+    var chars = Array(128).fill(0),                             // count target characters
+        f = [],                                                 // filtered list
+        flen,                                                   // filtered list length
+        tlen = t.length,                                        // number of target chars
+        a = '',                                                 // the a
+        head = 0,                                               // right pointer
+        tail = 0,                                               // left pointer
+        missing = 0;                                            // number of target chars missing
+                                                                // from the current window
 
 
+    // keep track of target characters in play
     for(let k = 0; k < t.length; k++) {
-        table[t.charCodeAt(k)]++;
-        count++;
+        chars[t.charCodeAt(k)]++;
+        missing++;
     }
 
-    log && l(table);
-
+    // Use a filtered list of characters, not the whole "s" string.
+    // "f" will contain ONLY the positions of the target characters
+    // in the "s" string. We will iterate over this filtered search
+    // space and not the full "s" length.
     for(let k = 0; k < s.length; k++) {
-        if(t.indexOf(s[k]) > -1) {
-            fs.push(k);
-        }
+        if(t.indexOf(s[k]) > -1) f.push(k);
     }
 
-    fslen = fs.length;
+    flen = f.length;
     
-    while(right < fslen) {
-        let nest = false;
+    // Slide across the search space  >>~-->
+    // Alternating movement of the head and tail positions
+    // gradually to the right.
+    while(head < flen) {
 
-        if(left > right) break;
+        if(tail > head) break;                              // Tail can't come before the head.
 
-        if(table[s.charCodeAt(fs[right])]-- > 0) count--;
+        if(chars[s.charCodeAt(f[head])]-- > 0) missing--;   // Account for the new, "head positioned"
+                                                            // target character. It is no longer "missing"
+                                                            // from the current solution candidate.
 
-        while(count === 0) {
+        // If we count that no target characters
+        // are missing, we have an answer.
+        while(missing === 0) {
+            a = !a                                      // if no answer is set, use the new one
+                || a.length > (f[head] - f[tail])       // if there's answer, but it's longer 
+                                                        // than our new one, use the new one
 
-            nest = true;
-            loop++;
-            if(left > right) break;
-
-            ans = 
-                !ans
-                || ans.length > (fs[right] - fs[left]) 
-                ? 
-                    s.slice(fs[left],(fs[right]+1))
-                    : ans;
+                    ?  s.slice(f[tail],(f[head]+1))     // <--<< new answer 
+                    : a;                                // doesn't fit the above conditions?
+                                                        // keep the old answer.
                 
-            if(ans.length === tlen) return ans;
+            if(a.length === tlen) return a;             // if our current answer is the same
+                                                        // size as the target string, we cannot
+                                                        // discover a better answer. return it.
 
-            left++;
-            if(table[s.charCodeAt(fs[left-1])]++ > -1) count++;
+            // contract the "tail position" toward the head...
+            tail++;
+
+            if(chars[s.charCodeAt(f[tail-1])]++ > -1) missing++;    // Since we "contracted" tail indward,
+                                                                    // we lost a target character from
+                                                                    // the tail end. We are now "missing"
+                                                                    // a target character.
         } 
 
-        loop = nest ? loop : (loop+1);
-        right++;
+        // expand the "head position" to the right...
+        head++;
     }
     
-    log && console.log('optmzd looped', loop);
-    return ans;
-};
-
-var minWindowNaive = function(s, t, log) {
-    loop = 0;
-    var table = {};
-    var slen = s.length;
-    var ans = '';
-    var right = 0;
-    var left = 0;
-    var count = 0;
-
-    t.split('').forEach((e) => {
-        if(table[e]) {
-            table[e]++;
-        } else {
-            table[e] = 1;
-        }
-        count++;
-    });
-
-
-    function updateRight(r) {
-        if(r in table) {
-            if(table[r]-- > 0) count--;
-        }
-    }
-    
-    while(right < slen) {
-        let nest = false;
-        if(left > right) break;
-
-        updateRight(s[right]);
-
-        while(count === 0) {
-
-            nest = true;
-            loop++;
-
-            if(left > right) break;
-
-            ans = !ans || ans.length > (right-left) ? s.slice(left,right+1) : ans;
-
-            left++;
-            let removed = s[left-1];
-            if(removed in table) {
-                if(table[removed]++ > -1) count++;
-            }
-        } 
-
-        loop = nest ? loop : (loop+1);
-        right++;
-    }
-    
-    log && console.log('naive looped', loop);
-    return ans;
+    return a;
 };
 
 var S = "ADOBECODEBAXNCYOUOUHIOABCIUOI";
-var T = "ABC";
-// var S = "acbbaca";
-// var T = "aba";
-// var S2 = "a";
-// var T2 = "aa";
+var T = "ABCY";
 
-let testNum = 1000000;
-var start = new Date().getTime();
-for(let j = 0; j < testNum; j++) {
-    if(j==0) {
-        l(minWindow(S,T,true)); continue;
-    }
-    minWindow(S, T);
-}
-var end = new Date().getTime();
-console.log(end-start);
-
-var start = new Date().getTime();
-for(let j = 0; j < testNum; j++) {
-    if(j==0) {
-        l(minWindowNaive(S,T,true)); continue;
-    }
-    minWindowNaive(S, T);
-}
-var end = new Date().getTime();
-console.log(end-start);
-//l(minWindow(S2, T2));
+console.log(minWindow(S, T));
